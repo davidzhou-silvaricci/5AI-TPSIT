63221d0e57593ee65b56af5c9cc41491cc40f388
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2022-03-10 18:26:42 +0100

    Add newly completed servlet

---
+++ src/main/java/com/davidzhou/servlet/RegistrazioneStudente.java
@@ -0,0 +1,46 @@
+package com.davidzhou.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegistrazioneStudente extends HttpServlet
+{
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        try {
+            this.processRequest(req, resp);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void processRequest(final HttpServletRequest req, final HttpServletResponse resp) throws SQLException, IOException, ClassNotFoundException
+    {
+        final String url = "jdbc:mariadb://localhost:3306/my_registro_voti";
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(url, "root", "");
+        
+        // Recupero dei dati dello studente
+        String[] parametri = new String[4];         // nome, cognome, email, sesso
+        parametri[0] = "'" + req.getParameter("nome") + "'";
+        parametri[1] = "'" + req.getParameter("cognome") + "'";
+        parametri[2] = "'" + req.getParameter("email") + "'";
+        parametri[3] = "'" + req.getParameter("sesso") + "'";
+        
+        String query = "INSERT INTO studente(nome, cognome, email, sesso) VALUES (" + String.join(", ", parametri) + ");";
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery(query);
+        
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>Registrazione avvenuta con successo!</body></html>");
+    }
+}
